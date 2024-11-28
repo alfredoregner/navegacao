30d083734e0684a19b111ee5512199f4d3c223db
Alfredo Regner <94856772+alfredoregner@users.noreply.github.com>
2024-11-28 19:35:47 -0300

    Finalizado estrutura de HTML + JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+// Direcionamentos
+function ex1(){
+    window.location = "exercicio1.html";
+}
+function ex2(){
+    window.location = "exercicio2.html";
+}
+function ex3(){
+    window.location = "exercicio3.html";
+}
+function ex4(){
+    window.location = "exercicio4.html";
+}
+function ex5(){
+    window.location = "exercicio5.html";
+}
+function home(){
+    window.location = "index.html";
+}
+
+// Exercício 1
 function verificar(){
 
     let numVariavel = parseInt(document.querySelector(".numVariavel").value);
@@ -12,6 +33,7 @@ function verificar(){
     window.location.reload();
 }
 
+// Exercício 2
 function bissexto(){
     let ano = parseInt(document.querySelector(".ano").value);
 
@@ -22,6 +44,7 @@ function bissexto(){
     }
 }
 
+// Exercício 3
 function nota(){
     let nota1 = parseFloat(document.querySelector(".nota1").value);
     let nota2 = parseFloat(document.querySelector(".nota2").value);
@@ -49,6 +72,7 @@ function nota(){
     window.location.reload();
 }
 
+// Exercício 4
 function calcular(){
     let conta = parseFloat(document.querySelector(".conta").value);
     let avaliacao = document.querySelector(".avaliacao").value;
@@ -74,6 +98,7 @@ function calcular(){
     window.location.reload();
 }
 
+// Exercício 5
 function hora(){
     let dia = new Date().getHours()
 
